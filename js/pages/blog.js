258b6d7da237d5029ba599e0725fc52b5dd843a2
Rena Hashimi <renhas54143@stud.noroff.no>
2024-01-23 12:43:36 +0100

    Update changes, css, new code in js

--- js/pages/blog.js
+++ js/pages/blog.js
@@ -5,8 +5,7 @@ const postBox = document.querySelector(".postbox");
 const pageName = document.querySelector(".pagename");
 const startAndSalad = document.querySelector(".ss");
 const mainCourse = document.querySelector(".mc");
-
-
+const filter = document.querySelector(".filter");
 
 export async function getPosts() {
    try {
@@ -14,12 +13,22 @@ export async function getPosts() {
     const posts = await response.json(); 
     console.log(posts);
     
+
+    filter.innerHTML = "";
     postBox.innerHTML = "";
     pageName.innerHTML = `<h1>Blog Posts</h1>
                           <p>“Where heritage meets the plate.”</p>`;
 
-
+   
     for (let i = 0; i <posts.length; i++) {
+
+        filter.innerHTML += `<form action="#" class="filterform">
+                                <select name="sort" id="sort"><i class="fa-solid fa-sort fa-2xl" style="color: #000000;"></i>
+                                    <option value="1">${posts[i].categories[0] === 3}</option>            
+                                    <option value="3">${posts[i].categories[0] === 6}</option>            
+                                    <option value="4"></option>            
+                                </select>
+                            </form> `;
         
         postBox.innerHTML += `<div class="postcontent">
                                <a href="blogpage.html?id=${posts[i].id}">
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,41 +1,44 @@
 import { errorMessage } from "../api/errormessage.js";
 import { BUTACUISINE_URL, BUTACUISINE_URL_MEDIA } from "../api/url.js";
-import { filterBtn } from "./sample.js";
 
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector (".pre")
 const slidePost = document.querySelector(".slidepost");
-const slidePost1 = document.querySelector(".slidepost1");
-const slidePost2 = document.querySelector(".slidepost2");
-const slidePost3 = document.querySelector(".slidepost3");
-const slide = Array.from(slidePost.children);
+const slidePost1 = document.querySelector("#slidepost1");
+const slidePost2 = document.querySelector("#slidepost2");
+const slidePost3 = document.querySelector("#slidepost3");
  
-const postWidth = slide[0].getBoundingClientRect().width;
-console.log(postWidth);
 
-// slide[0].style.left = 0;
-// slide[1].style.left = postWidth * 1 + "px";
-// slide[2].style.left = postWidth * 2 + "px";
+// REFERENCE FOR CAROUSEL: https://youtu.be/9HcxHDS2w1s?si=AlX06ovT6-FNpefE
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
+//END OF VIDEO - REFERENCE FOR CAROUSEL
+
+
+
+
+
 async function carousel() {
     try{
         const response = await fetch(BUTACUISINE_URL_MEDIA);
         const posts = await response.json();
         console.log(posts);
-
-/* POST POSITION */
-slide.forEach((slides, index) =>{
-    slides.style.left = postWidth * index + "px";
-});
-
-nextBtn.addEventListener("click", e =>{
-    const activeSlide = document.querySelector(".active-slide");
-    const nextslide = activeSlide.nextElementSibling;
-    const moveSlide = nextslide.style.left;
-    filterBtn();
-    slidePost.style.transform = "translateX(-" + moveSlide + ")";
-    console.log(activeSlide);
-})
-
 
 
         slidePost1.innerHTML = "";
@@ -44,10 +47,12 @@ nextBtn.addEventListener("click", e =>{
 
     for (let i = 0; i <posts.length; i++) {
         if (posts[i].id === 8){
-            slidePost1.innerHTML += `<div class="postcontent">
+            slidePost1.innerHTML += `<div class="postcontent2">
                                <a href="blogpage.html?id=${posts[i].id}">
                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-                                <h2 class="poststitle">${posts[i].title.rendered}</h2>
+                                <h3 class="poststitle">${posts[i].title.rendered}</h3>
+                                <p class="shorttext">${posts[i].excerpt.rendered}</p>
+                                <li class="carlink">Go to the recipe</li>
                                 </a>
                               </div>`;
         }  
@@ -55,10 +60,12 @@ nextBtn.addEventListener("click", e =>{
     
     for (let i = 0; i <posts.length; i++) {
         if (posts[i].id === 124){
-            slidePost2.innerHTML += `<div class="postcontent">
+            slidePost2.innerHTML += `<div class="postcontent2">
                                <a href="blogpage.html?id=${posts[i].id}">
                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-                                <h2 class="poststitle">${posts[i].title.rendered}</h2>
+                                <h3 class="poststitle">${posts[i].title.rendered}</h3>
+                                <p class="shorttext">${posts[i].excerpt.rendered}</p>
+                                <li class="carlink">Go to the recipe</li>
                                 </a>
                               </div>`;
         } 
@@ -66,11 +73,12 @@ nextBtn.addEventListener("click", e =>{
 
     for (let i = 0; i <posts.length; i++) {
         if (posts[i].id === 20){
-            slidePost3.innerHTML += `<div class="postcontent">
+            slidePost3.innerHTML += `<div class="postcontent2">
                                <a href="blogpage.html?id=${posts[i].id}">
                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-                                <h2 class="poststitle">${posts[i].title.rendered}</h2>
-                                <button class="r-m-btn">Read more</button>
+                                <h3 class="poststitle">${posts[i].title.rendered}</h3>
+                                <p class="shorttext">${posts[i].excerpt.rendered}</p>
+                                <li class="carlink">Go to the recipe</li>
                                 </a>
                               </div>`;
         } 
@@ -79,7 +87,7 @@ nextBtn.addEventListener("click", e =>{
 } catch(error) {
     console.log("Unknown error", error);
     // .innerHTML = errorMessage();
-}
+  }
 }
 
 carousel()
