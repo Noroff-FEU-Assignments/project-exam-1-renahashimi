2ec4bb1b1709d782efcac0acaa4903749f6ea83f
Rena Hashimi <renhas54143@stud.noroff.no>
2024-02-02 23:28:38 +0100

    carousel js - css

--- js/pages/index.js
+++ js/pages/index.js
@@ -10,26 +10,15 @@ console.log(nextBtn);
 const prevBtn = document.querySelector (".carbtnpre");
 const carouselContainer = document.querySelector(".carousel");
 
-/* WELCOME */
 
-const welcomeImg = document.querySelector(".welcomeimg");
-const welcomeTxt = document.querySelector(".welcometxt");
-
-welcomeImg.innerHTML += `<img src="/images/Pomegranate.png" alt="Pomegranate">`;
-welcomeTxt.innerHTML += `<h3>Welcome</h3>
-                          <p>to my</p>
-                          <p class="foodbold"><span>food</span> blog</p>
-                          <a href="blog.html">All blogs</a>`;    
-
-
-
-async function carousel() {
+async function fecthIndexData() {
   try {
       const response = await fetch(carouselUrl);
       const posts = await response.json();
       console.log(posts);
-     
-      carouselContent(posts);
+
+      setupWelcomeSection();
+      displayCarousel(posts);
       carouselContainer.innerHTML += "";
       
 
@@ -37,11 +26,26 @@ async function carousel() {
     console.log("Something went wrong", error);
   }
 }
-carousel();
 
-function carouselContent(posts) {
-  posts.forEach(function (posts) {
-    carouselContainer.innerHTML += `<div class="carouselcontent">
+/* WELCOME */
+
+function setupWelcomeSection(){
+const welcomeImg = document.querySelector(".welcomeimg");
+const welcomeTxt = document.querySelector(".welcometxt");
+  
+  welcomeImg.innerHTML += `<img src="/images/Pomegranate.png" alt="Pomegranate">`;
+  welcomeTxt.innerHTML += `<h3>Welcome</h3>
+                            <p>to my</p>
+                            <p class="foodbold"><span>food</span> blog</p>
+                            <a href="blog.html">All blogs</a>`;    
+
+}
+
+/* CAROUSEL */
+
+function displayCarousel(posts) {
+  posts.forEach((posts) => {
+    const indexPost = `<div class="carouselcontent">
                                       <a href="blogpage.html?id=${posts.id}" class="caritem">
                                         <img class="carimage" src="${posts._embedded["wp:featuredmedia"][0].source_url}" alt="${posts.title.rendered}">
                                         <div class="caritemblock">
@@ -55,71 +59,47 @@ function carouselContent(posts) {
                                         </div> 
                                       </a>
                                     </div>`;
+carouselContainer.innerHTML += indexPost;
   }); 
 }
 
-  nextBtn.addEventListener("click", function() {
-    carouselContainer.style.animation = "slides 14s forwards";
+
+let currentIndex = 0;
+const posts = document.querySelectorAll(".carouselcontent");
+
+const allPosts = posts.length;
+
+function carouselSlide(index) {
+  const offset = -100 * index;
+    carouselContainer.style.transform = `translateX(${offset}%)`; 
+  };
+  
+nextBtn.addEventListener("click", function(){
+  carouselContainer.style.transform = "translateX(-100%)";
+  currentIndex = (currentIndex + 1)
+  carouselSlide(currentIndex);
+
+  if (currentIndex === posts.length -1) {
+    nextBtn.classList.add("disabled");
+  } else {
+    prevBtn.classList.remove("disabled");
+  }
+
+
+});
+prevBtn.addEventListener("click", function(){
+  carouselContainer.style.transform = "translateX(0)";
+  currentIndex = (currentIndex - 1)
+  carouselSlide(currentIndex);
+
+    
+  if (currentIndex === 0) {
+    prevBtn.classList.add("disabled");
+  } else {
+    nextBtn.classList.remove("disabled");
+  }
+
 });
 
-prevBtn.addEventListener("click", function() {
-  carouselSpinner.style.animation = "slides 14s forwards";
-});
 
-// prevBtn.addEventListener("click", () => {
-//   carouselContainer.scrollLeft += -1000;
-// });
-// nextBtn.addEventListener("click", () => {
-//   carouselContainer.scrollLeft += 1000;
-// });
-
-// REFERENCE FOR CAROUSEL: https://youtu.be/9HcxHDS2w1s?si=AlX06ovT6-FNpefE
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-//     const slides = button
-//       .closest("[data-carousel]")
-//       .querySelector("[data-slides]");
-
-//     const activeSlide = slides.querySelector("[data-active]");
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-//     if (newIndex < 0) newIndex = slides.children.length - 1;
-//     if (newIndex >= slides.children.length) newIndex = 0;
-
-//     slides.children[newIndex].dataset.active = true;
-//     delete activeSlide.dataset.active;
-//   });
-// });
-// //END OF VIDEO - REFERENCE FOR CAROUSEL
-
-//       for (let i = 0; i <posts.length; i++) {
-//         if (posts[i].id === 149){
-//             slidePost1.innerHTML += `<div class="postcontent">
-//                                <a href="blogpage.html?id=${posts[1].id}">
-//                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-//                                 <h2 class="poststitle">${posts[i].title.rendered}</h2>
-//                                 <p class="shorttext">${posts[i].excerpt.rendered}</p>
-//                                 <li class="carlink">Go to the recipe</li>
-//                                 </a>
-//                               </div>`;
-//         } if (posts[i].id === 157){
-//             slidePost2.innerHTML += `<div>
-//                                <a href="blogpage.html?id=${posts[i].id}">
-//                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-//                                 <h3 class="poststitle">${posts[i].title.rendered}</h3>
-//                                 <p class="shorttext">${posts[i].excerpt.rendered}</p>
-//                                 <li class="carlink">Go to the recipe</li>
-//                                 </a>
-//                               </div>`;
-//         } if (posts[i].id === 20){
-//             slidePost3.innerHTML += `<div>
-//                                <a href="blogpage.html?id=${posts[i].id}">
-//                                 <img class="postsimage" src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-//                                 <h3 class="poststitle">${posts[i].title.rendered}</h3>
-//                                 <p class="shorttext">${posts[i].excerpt.rendered}</p>
-//                                 <li class="carlink">Go to the recipe</li>
-//                                 </a>
-//                               </div>`;
-//         } 
-   
-
+fecthIndexData()
